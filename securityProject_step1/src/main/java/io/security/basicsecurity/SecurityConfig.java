563acd111b8027be628937ece03098f5d9677436
SeungEune <dltmddms4860@gmail.com>
2022-09-13 23:38:21 +0900

    스프링 시큐리티 기본 API 및 Filter 이해 - 11강) 권한설정과 표현식

--- securityProject_step1/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ securityProject_step1/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -2,7 +2,9 @@ package io.security.basicsecurity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,8 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     UserDetailsService userDetailsService;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    /*인증 관련 API Start*/
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
 //
 ////      인가 초기화
 //        http
@@ -59,38 +62,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
         /*로그아웃 처리 관련*/
-        http
-                .authorizeRequests()
-                .anyRequest().authenticated();
-
-        http
-                .formLogin();
-
-        http
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")/*logoutSuccessHandler와 다른 점은 이건 단순 Url 이동만 담당함.*/
-                .addLogoutHandler(new LogoutHandler() {
-                    @Override
-                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-                        HttpSession session = request.getSession();
-                        session.invalidate();
-                    }
-                })
-                .logoutSuccessHandler(new LogoutSuccessHandler() {
-                    @Override
-                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        response.sendRedirect("/login");
-                    }
-                });
-
-        /*remember-me 기능 관련*/
-        http
-                .rememberMe()
-                .rememberMeParameter("remember") //remember-me 체크박스 테그 name을 같게 해줘야한다.
-                .tokenValiditySeconds(3600)
-                .alwaysRemember(false) //remember-me 기능 체크박스가 활성화 되지 않아도 항상 실행 여부
-                .userDetailsService(userDetailsService) //스프링부트 3.7.3에서는 userDetailesService가 바뀜...?
+//        http
+//                .authorizeRequests()
+//                .anyRequest().authenticated();
+//
+//        http
+//                .formLogin();
+//
+//        http
+//                .logout()
+//                .logoutUrl("/logout")
+//                .logoutSuccessUrl("/login")/*logoutSuccessHandler와 다른 점은 이건 단순 Url 이동만 담당함.*/
+//                .addLogoutHandler(new LogoutHandler() {
+//                    @Override
+//                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+//                        HttpSession session = request.getSession();
+//                        session.invalidate();
+//                    }
+//                })
+//                .logoutSuccessHandler(new LogoutSuccessHandler() {
+//                    @Override
+//                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+//                        response.sendRedirect("/login");
+//                    }
+//                });
+//
+//        /*remember-me 기능 관련*/
+//        http
+//                .rememberMe()
+//                .rememberMeParameter("remember") //remember-me 체크박스 테그 name을 같게 해줘야한다.
+//                .tokenValiditySeconds(3600)
+//                .alwaysRemember(false) //remember-me 기능 체크박스가 활성화 되지 않아도 항상 실행 여부
+//                .userDetailsService(userDetailsService) //스프링부트 3.7.3에서는 userDetailesService가 바뀜...?
 //                .rememberMeCookieName("cookie rename")/*쿠키 명 변경 메소드*/
         ;
 
@@ -112,12 +115,63 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
         /*세션 정책*/
-        http
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+//        http
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
         //SessionCrestionPolicy.Always        : 스프링 시큐리트가 항상 세션 생성
         //SessionCreationPolicy.If_Required   : 스프링 시큐리티가 필요 시 생성(기본값)
         //SessionCreationPoilcy.Never         : 스프링 시큐리티가 생성하지 않지만 이미 존재하면 사용
         //SessionCreationPolicy.Stateless     : 스프링 시큐리티가 생성하지 않고 존재해도 사용하지 않음 -> JWT(Json Web Token)사용할 때 사용
+//    }
+    /*인증 관련 API END*/
+
+
+    /*인가 관련 API Start*/
+    //메모리에 사용자를 설정함.
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS", "USER");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN", "SYS", "USER");
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .antMatcher("/shop/**")
+//                .authorizeRequests()
+//                    .antMatchers("/shop/login", "/shop/users/**").permitAll()
+//                    .antMatchers("/shop/mypage").hasRole("USER")
+//                    .antMatchers("/shop/admin/pay").access("hasRole('ADMIN')")
+//                    .antMatchers("/shop/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
+//                    .anyRequest().authenticated(); //<- 해당 조건은 위 조건을 제외한 모든 요청에 대해선 인증을 받은 사용자만이 접근 할 수 있다.
+        //주의 사항 : 설정 시 구체적인 경로가 먼저 오고 그것 보다 큰 범위의 경로가 뒤에 오도록 해야한다.
+
+        /*인가 API 표현식*/
+//        authenticated() 인증된 사용자의 접근을 허용
+//        fullyAuthenticated() 인증된 사용자의 접근을 허용, rememberMe 인증 제외
+//        permitAll() 무조건 접근을 허용
+//        denyAll() 무조건 접근을 허용하지 않음
+//        anonymous() 익명사용자의 접근을 허용
+//        rememberMe() 기억하기를 통해 인증된 사용자의 접근을 허용
+//        access(String) 주어진 SpEL 표현식의 평가 결과가 true이면 접근을 허용
+//        hasRole(String) 사용자가 주어진 역할이 있다면 접근을 허용
+//        hasAuthority(String) 사용자가 주어진 권한이 있다면
+//        hasAnyRole(String...) 사용자가 주어진 권한이 있다면 접근을 허용
+//        hasAnyAuthority(String...) 사용자가 주어진 권한 중 어떤 것이라도 있다면 접근을 허용
+//        hasIpAddress(String) 주어진 IP로부터 요청이 왔다면 접근을 허용
+        /*인가 API 표현식*/
+
+
+        http
+                .authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
+                .anyRequest().authenticated();
+
+        http
+                .formLogin();
+    }
+    /*인가 관련 API End*/
 }
